c2e421ac711f815763174d03ad27ba17b1d0bf11
Deya Genao <deyagenao@gmail.com>
2019-08-14 21:27:12 -0400

    updated basic css

---
+++ assets/javascript/app.js
@@ -0,0 +1,40 @@
+// Global Variables 
+//===========================================================================================================================
+//Reference for the timer 
+
+//Counter for correct answers 
+
+//Counter for incorrect answers 
+
+//Counter for unanswered questions 
+
+
+
+
+
+//Function Definitions 
+//===========================================================================================================================
+//Function for the timer that runs for each question 
+
+//On-click function for the start button, first question will be displayed, timer will start to count down 
+
+//if timer runs out, display incorrect answer screen 
+
+//On-click functions for multiple choice options 
+//Correct choice
+//If correct choice made, timer pauses, and the correct answer screen is displayed
+    //only happens if timer is greater than 0
+//After 6 seconds, display next question 
+//Incorrect choice 
+//If incorrect choice made, timer pauses, and the incorrect answer screen is displayed
+    //only happens if timer is greater than 0  
+//After 6 seconds, display next question 
+
+//Display the total number correct at the end of the game 
+
+//On-click function for restarting the game 
+
+
+
+//Function Calls 
+//===========================================================================================================================
