8d9b0b1c4152b5e984b6029884ab613caa6a5a08
Deya Genao <deyagenao@gmail.com>
2019-08-17 12:21:10 -0400

    Last commit before attempting a new method for implementing the javascript, which includes a separate js file with the trivia questions

---
+++ assets/javascript/GOT-trivia-questions.js
@@ -0,0 +1,17 @@
+var question1 = {
+    question: 'The Braavos saying "Valur Morghulis" translates to...',
+    choices: ["All men must serve", "Praise the Many Faced God", "All men must die", "Praise the Braavosi"],
+    correctAnswer: "All men must die",
+    correctAnswerGifUrl: "",
+}
+
+// Who was responsible for the death of Rob Stark and ?
+// Daenerys Targarean is referred to all of the following, EXCEPT... 
+// After traveling North of the Wall, Brandon Stark becomes ...
+// Who was responsible for defeating the Night King 
+
+var question10 = {
+    question: 'Complete this famous line: "When you play the Game of Thrones..."',
+    choices: ["A) you fly, or you fry.", "B) everyone is your enemy.", "C) you answer to no one.", "D) you win, or you die."],
+    correctAnswer: "D) you win, or you die.",
+}
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,16 @@
 // Global Variables 
 //===========================================================================================================================
 //Reference for the timer 
-
+var timeRemainingSeconds = 60;
+var intervalId;
+//Reference for showing the answer screen only for a given amount of time
+var displayAnswerTimeout;
 //Counter for correct answers 
-
+var correctAnswers = 0;
 //Counter for incorrect answers 
-
+var incorrectAnswers = 0;
 //Counter for unanswered questions 
-
-
+var unanswered = 0;
 
 
 
@@ -17,18 +19,44 @@
 //Function for the timer that runs for each question 
 
 //On-click function for the start button, first question will be displayed, timer will start to count down 
+$("#start-button").on("click", function(){
+    $("#start-game").hide();
+    $("#time-remaining").show();
+    $("#question-1").show();
+    //if timer runs out, display incorrect answer screen 
 
-//if timer runs out, display incorrect answer screen 
+})
 
 //On-click functions for multiple choice options 
 //Correct choice
 //If correct choice made, timer pauses, and the correct answer screen is displayed
     //only happens if timer is greater than 0
-//After 6 seconds, display next question 
+$("#correct-choice-1").on("click", function(){
+    correctAnswers++;
+    $("#time-remaining").hide();
+    $("#question-1").hide();
+    $("#answer-1").show();
+    //After 5 seconds, display next question 
+    displayAnswerTimeout = setTimeout(function() {
+        $("#answer-1").hide();
+        $("#question-2").show();
+        }, 5000); 
+}); 
 //Incorrect choice 
 //If incorrect choice made, timer pauses, and the incorrect answer screen is displayed
     //only happens if timer is greater than 0  
-//After 6 seconds, display next question 
+
+$(document).on("click", ".incorrect-choice-1", function(){
+    incorrectAnswers++;
+    $("#time-remaining").hide();
+    $("#question-1").hide();
+    $("#answer-1").show();
+    //After 5 seconds, display next question 
+    displayAnswerTimeout = setTimeout(function() {
+        $("#answer-1").hide();
+        $("#question-2").show();
+        }, 5000); 
+});
 
 //Display the total number correct at the end of the game 
 
