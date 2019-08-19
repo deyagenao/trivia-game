e1e362db6eb266328e6c257b6b4580e3b0983aba
Deya Genao <deyagenao@gmail.com>
2019-08-18 23:23:24 -0400

    completed trivia questions and added images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,6 +13,68 @@ var incorrectAnswers = 0;
 var unanswered = 0;
 // Counter for the current question number 
 var questionCounter = 0;
+// Questions 
+var questions = [{
+    question: '1) The Braavos saying "Valar Morghulis" translates to...',
+    choices: ["a) All men must serve", "b) Praise the Many Faced God", "c) All men must die", "d) Praise the Braavosi"],
+    correctAnswer: "c) All men must die",
+    correctAnswerGifUrl: "assets/images/valar-morhulis-gif.webp",
+},
+{
+    question: "2) Who was responsible for the death of Robb and Catelyn Stark?",
+    choices: ["a) The Lannisters", "b) The Greyjoys", "c) The Baratheons", "d) The Freys"],
+    correctAnswer: "d) The Freys",
+    correctAnswerGifUrl: "assets/images/frey.gif",
+},
+{
+    question: "3) Which of the following is not part of Daenerys Targaryen's full title?",
+    choices: ["a) The Silver-haired", "b) The Mother of Dragons", "c) The Breaker of Chains", "d) The Queen of the Andals"],
+    correctAnswer: "a) The Silver-haired",
+    correctAnswerGifUrl: "assets/images/daenerys.gif",
+},
+{
+    question: "4) After traveling North of the Wall, Brandon Stark becomes ...",
+    choices: ["a) Bran the Broken", "b) A Whitewalker ", "c) The Three-Eyed Raven", "d) One of the Free Folk"],
+    correctAnswer: "c) The Three-Eyed Raven",
+    correctAnswerGifUrl: "assets/images/bran-stark.gif",
+},
+{
+    question: "5) What discovery does Samwell Tarly make while at the Citadel?",
+    choices: ["a) Jaime Lannister fathered Cersei's children", "b) Jon Snow's true parents are Lyanna Stark and Rhaegar Targaryen", "c) Tyrion Lannister murdered his father", "d) Cersei Lannister used wildfire to destroy the Sept of Baelor"],
+    correctAnswer: "b) Jon Snow's true parents are Lyanna Stark and Rhaegar Targaryen",
+    correctAnswerGifUrl: "assets/images/samwell-tarly.gif",
+},
+{
+    question: "6) Who did Arya Stark train to serve?",
+    choices: ["a) The Lord of Light", "b) The New Gods - The Seven ", "c) The Horse God", "d) The Many-Faced God"],
+    correctAnswer: "d) The Many-Faced God",
+    correctAnswerGifUrl: "assets/images/many-faced.gif",
+},
+{
+    question: "7) Who is Roberty Baratheon's last heir?",
+    choices: ["a) Tommen", "b) Joffrey", "c) Gendry", "d) Myrcella"],
+    correctAnswer: "c) Gendry",
+    correctAnswerGifUrl: "assets/images/gendry.gif",
+},
+{
+    question: "8) At the end of the series, the rightful leader in the North is...",
+    choices: ["a) Jon Snow", "b) Sansa Stark", "c) Daenerys Targaryen", "d) Brandon Stark"],
+    correctAnswer: "b) Sansa Stark",
+    correctAnswerGifUrl: "assets/images/sansa.gif",
+},
+{
+    question: "9) Who defeated the Night King?",
+    choices: ["a) Arya Stark", "b) Jon Snow", "c) Daenerys Targaryen", "d) Brandon Stark"],
+    correctAnswer: "a) Arya Stark",
+    correctAnswerGifUrl: "assets/images/night-king.gif",
+},
+{
+    question: '10) Complete this famous line: "When you play the Game of Thrones..."',
+    choices: ["a) you fly, or you fry.", "b) everyone is your enemy.", "c) you answer to no one.", "d) you win, or you die."],
+    correctAnswer: "d) you win, or you die.",
+    correctAnswerGifUrl: "assets/images/cersei.gif",
+}
+]
 
 // Function Definitions & Calls
 //===========================================================================================================================
@@ -24,15 +86,18 @@ function runTimeLeft() {
     intervalID = setInterval(decrement, 1000);
 }
 // Decrement function that will actually decrease the number of seconds and display on the screen
-function decrement () {
-    //decrease seconds by 1 
-    timeRemainingSeconds--;
+function decrement() {
     //display the number in the "time remaining text" box
     $("#time-remaining").text("Time Remaining: " + timeRemainingSeconds);
+    //decrease seconds by 1 
+    timeRemainingSeconds--;
     // when the time remaining reaches 0 seconds, then stop 
     if (timeRemainingSeconds === 0) {
-        clearInterval(intervalID);
+        stopTimer();
     }
+}
+var stopTimer = function() {
+    clearInterval(intervalId);
 }
 /////////////////
 // Attaching an event handler (function) to the start button
@@ -46,6 +111,10 @@ $("#start-button").on("click", function() {
 /////////////////
 // function for displaying each question
 var displayQuestion = function () {
+    // display the timer to show the time left to answer the question
+    $("#time-remaining").show();
+    // begin the timer 
+    runTimeLeft();
     // display the question on the page with h3 formatting
     $("#question-element").html("<h3>" + questions[questionCounter].question + "</h3>");
     // display the choices for the question
@@ -57,10 +126,6 @@ var displayQuestion = function () {
         // appending each new paragraph with each multiple choice to the choices element to display on the page 
         $("#choices-element").append(newChoice);
     }
-    // display the timer to show the time left to answer the question
-    $("#time-remaining").show();
-    // begin the timer 
-    runTimeLeft();
     // if the timer reaches 0 seconds, automatically move onto displaying the answer for the current question
     
 }
@@ -72,15 +137,16 @@ var displayAnswer = function() {
     $("#question-element").empty();
     // clear the choices from the display
     $("#choices-element").empty();
-    // hide the timer 
-    $("#time-remaining").hide();
+    // stop the timer
+    stopTimer();
     // displaying the correct answer for the user 
     $("#correct-answer-element").text(
       "The correct answer was: " + questions[questionCounter].correctAnswer
     );
-    // var answerImage = $("<img>");
-    // answerImage.attr("src", "questions[questionCounter].correctAnswer");
-    // $("#answer-gif-element").append(answerImage);
+    var answerImage = $("<img>");
+    answerImage.attr("src", questions[questionCounter].correctAnswerGifUrl);
+    answerImage.addClass("answer-image");
+    $("#answer-gif-element").append(answerImage);
 
     // function to set a timeout for displaying the answer and automatically moving onto the next question after 4 seconds 
     displayAnswerTimeout = setTimeout(function() {
